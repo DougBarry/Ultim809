581918fd1f6b06eab93ad8dd2abf4de33103a444
dougbarry <github@gtko.co.uk>
2016-07-10 22:18:09 +0100

    Fixes to code for Python 3 compat. Not fully tested.

--- code/ser09.py
+++ code/ser09.py
@@ -4,6 +4,9 @@
 # Matt Sarnoff (msarnoff.org/6809)
 # December 15, 2010
 #
+# Modifications for generic ROM burning by Doug Barry 20160710
+# Also upgraded to Python 3.5 syntax.
+#
 # Usage:
 #  ser09 load file.s19  load file.s19 into memory (RAM or ROM)
 #  ser09 run file.s19   load file.s19 into memory and execute it
@@ -12,7 +15,7 @@
 # The first serial port is used by default. Can be overridden with the
 # -p option or the SER09_PORT environment variable.
 
-import getopt, sys, serial
+import getopt, sys, serial, struct
 from os import getenv
 from time import sleep
 
@@ -39,155 +42,178 @@ FN_SUCCESS			= b'\x01'
 
 # --------------------
 class Group:
-	"""splits a sequence into groups of elements"""
-	def __init__(self,l,size):
-		self.size = size
-		self.l = l
-	
-	def __getitem__(self, group):
-		idx = group * self.size
-		if idx >= len(self.l):
-			raise IndexError("Out of range")
-		return self.l[idx:idx+self.size]
-	
+    """splits a sequence into groups of elements"""
+    def __init__(self,l,size):
+        self.size = size
+        self.l = l
+
+    def __getitem__(self, group):
+        idx = group * self.size
+        if idx >= len(self.l):
+            raise IndexError("Out of range")
+        return self.l[idx:idx+self.size]
+
 
 # --------------------
 class MemImage:
-	"""represents a memory image as a sequence of bytes"""
-	def __init__(self):
-		self.bytes = bytearray()
-		self.startAddr = 0
-		self.startAddrSet = False
-		self.currentAddr = 0
+    """represents a memory image as a sequence of bytes"""
+    def __init__(self):
+        self.bytes = bytearray()
+        self.startAddr = 0
+        self.startAddrSet = False
+        self.currentAddr = 0
 
-	def setAddress(self, newaddr):
-		if not self.startAddrSet:
-			self.startAddr = newaddr
-			self.currentAddr = newaddr
-			self.startAddrSet = True
-		else:
-			if newaddr < self.currentAddr:
-				raise Exception("Backward jumps not allowed")
-			elif newaddr > self.currentAddr:
-				# fill intermediate bytes with 0xFF
-				self.bytes.extend(b"\xFF"*(newaddr-self.currentAddr))
-				self.currentAddr = newaddr
-	
-	def appendByte(self, byte):
-		self.bytes.append(byte)
-		self.currentAddr += 1
-		
-	def length(self):
-		return len(self.bytes)
-	
-	def endAddr(self):
-		return self.startAddr + self.length()
-		
-	@staticmethod
-	def fromS19File(filename):
-		f = open(filename)
-		mem = MemImage()
-		for line in f:
-			recordtype = line[0:2]
-			if recordtype == "S0":		# don't care about S0 records
-				pass
-			elif recordtype == "S9":	# don't care about S9 records
-				pass
-			elif recordtype == "S1":	# only care about S1 records
-				line = line.rstrip()
-				bytecount = int(line[2:4],16)
-				addr = int(line[4:8],16)
-				databytes = line[8:-2]
-				checksum = int(line[-2:],16)
-				thischecksum = bytecount + ((addr>>8)&0xFF) + (addr&0xFF)
-				mem.setAddress(addr)
-				for hexbyte in Group(databytes,2):
-					byteval = int(hexbyte,16)
-					mem.appendByte(byteval)
-					thischecksum += byteval
-				thischecksum = (~thischecksum)&0xFF
-				if thischecksum != checksum:
-					raise Exception("Bad checksum")
-			else:											# anything else isn't supported
-				raise Exception("'%s' record not supported" % recordtype)
-		return mem
-			
+    def setAddress(self, newaddr):
+        if not self.startAddrSet:
+            self.startAddr = newaddr
+            self.currentAddr = newaddr
+            self.startAddrSet = True
+        else:
+            if newaddr < self.currentAddr:
+                raise Exception("Backward jumps not allowed")
+            elif newaddr > self.currentAddr:
+                # fill intermediate bytes with 0xFF
+                self.bytes.extend(b"\xFF"*(newaddr-self.currentAddr))
+                self.currentAddr = newaddr
+
+    def appendByte(self, byte):
+        self.bytes.append(byte)
+        self.currentAddr += 1
+
+    def length(self):
+        return len(self.bytes)
+
+    def endAddr(self):
+        return self.startAddr + self.length()
+
+    @staticmethod
+    def fromS19File(filename):
+        f = open(filename)
+        mem = MemImage()
+        for line in f:
+            recordtype = line[0:2]
+            if recordtype == "S0":		# don't care about S0 records
+                pass
+            elif recordtype == "S9":	# don't care about S9 records
+                pass
+            elif recordtype == "S1":	# only care about S1 records
+                line = line.rstrip()
+                bytecount = int(line[2:4],16)
+                addr = int(line[4:8],16)
+                databytes = line[8:-2]
+                checksum = int(line[-2:],16)
+                thischecksum = bytecount + ((addr>>8)&0xFF) + (addr&0xFF)
+                mem.setAddress(addr)
+                for hexbyte in Group(databytes,2):
+                    byteval = int(hexbyte,16)
+                    mem.appendByte(byteval)
+                    thischecksum += byteval
+                thischecksum = (~thischecksum)&0xFF
+                if thischecksum != checksum:
+                    raise Exception("Bad checksum")
+            else:											# anything else isn't supported
+                raise Exception("'%s' record not supported" % recordtype)
+        return mem
+
 
 # --------------------
 def romLoadStart(ser):
-	ser.write(FN_ROMLD_START)
-	if ser.read(1) != FN_SUCCESS:
-		raise Exception("FN_ROMLD_START command failed")
+    ser.write(FN_ROMLD_START)
+    if ser.read(1) != FN_SUCCESS:
+        raise Exception("FN_ROMLD_START command failed")
 
 # --------------------
 def romLoadDone(ser):
-	ser.write(FN_ROMLD_DONE)
+    ser.write(FN_ROMLD_DONE)
 
 # --------------------
 def writeMem(ser, addr, chunk):
-	addrhi = (addr>>8)&0xFF
-	addrlo = addr&0xFF
-	numbytes = len(chunk)
-	ser.write(bytearray([FN_WRITE_MEM, addrhi, addrlo, numbytes]))
-	ser.write(chunk)
-	if ser.read(1) != FN_SUCCESS:
-		raise Exception("FN_WRITE_MEM command failed")
+    addrhi = (addr>>8)&0xFF
+    addrlo = addr&0xFF
+    numbytes = len(chunk)
+    print(addrhi)
+    print(addrlo)
+    print(numbytes)
+    packet = bytearray()
+
+    packet.extend(FN_WRITE_MEM)
+    packet.extend(struct.pack("H", addrhi))
+    packet.extend(struct.pack("H", addrlo))
+    packet.extend(struct.pack("I", numbytes))
+
+    result = ser.write(packet)
+    ser.write(chunk)
+    if ser.read(1) != FN_SUCCESS:
+        raise Exception("FN_WRITE_MEM command failed")
 
 # --------------------
 def readMem(ser, addr, numbytes):
-	addrhi = (addr>>8)&0xFF
-	addrlo = addr&0xFF
-	ser.write(bytearray([FN_READ_MEM, addrhi, addrlo, numbytes]))
-	return bytearray(ser.read(numbytes))
+    addrhi = (addr >> 8) & 0xFF
+    addrlo = addr&0xFF
+
+    packet=bytearray()
+
+    packet.extend(FN_READ_MEM)
+    packet.extend(struct.pack("H", addrhi))
+    packet.extend(struct.pack("H", addrlo))
+    packet.extend(struct.pack("I", numbytes))
+
+    print("writing: ", packet)
+    ser.write(packet)
+
+    print("waiting for reply []", numbytes)
+    return bytearray(ser.read(numbytes))
+
 
 # --------------------
 def readAndPrintMem(ser, addr, numbytes):
-	bytes = readMem(ser, addr, numbytes)
-	for bytegroup in Group(bytes, 16):
-		print "0x%04X:" % addr,
-		for byte in bytegroup:
-			print "%02X" % byte,
-		print
-		addr += 16
+    bytes = readMem(ser, addr, numbytes)
+    for bytegroup in Group(bytes, 16):
+        print("0x%04X:" % addr),
+        for byte in bytegroup:
+            print("%02X" % byte),
+        print
+        addr += 16
+
 
 # --------------------
 def execAtAddress(ser, addr):
-	addrhi = (addr>>8)&0xFF
-	addrlo = addr&0xFF
-	ser.write(bytearray([FN_RUN_TARGET, addrhi, addrlo]))
+    addrhi = (addr>>8)&0xFF
+    addrlo = addr&0xFF
+    ser.write(bytearray([FN_RUN_TARGET, addrhi, addrlo]))
 
 
 # --------------------
 def sendData(ser, memimage, isRom):
-	# if sending a rom image, machine must relocate its monitor
-	if isRom:
-		print "Preparing to upload ROM image"
-		romLoadStart(ser)
-		print "OK"
-	# now send the byte chunks
-	addr = memimage.startAddr
-	for chunk in Group(memimage.bytes,PAGE_SIZE):
-		numbytes = len(chunk)
-		print "Writing %d byte(s) to 0x%04X" % (numbytes, addr)
-		# send the data and wait for acknowledge
-		writeMem(ser, addr, chunk)
-		# read the data back
-		response = readMem(ser, addr, numbytes)
-		if response != chunk:
-			print repr(chunk)
-			print repr(response)
-			raise Exception("Chunk verify failed")
-		addr += numbytes
-	# finish the rom transfer if necessary
-	if isRom:
-		print "Finishing ROM upload"
-		romLoadDone(ser)
-	print "All OK"
+    # if sending a rom image, machine must relocate its monitor
+    if isRom:
+        print("Preparing to upload ROM image")
+        romLoadStart(ser)
+        print("OK")
+    # now send the byte chunks
+    addr = memimage.startAddr
+    for chunk in Group(memimage.bytes,PAGE_SIZE):
+        numbytes = len(chunk)
+        print("Writing %d byte(s) to 0x%04X" % (numbytes, addr))
+        # send the data and wait for acknowledge
+        writeMem(ser, addr, chunk)
+        # read the data back
+        response = readMem(ser, addr, numbytes)
+        if response != chunk:
+            print(repr(chunk))
+            print(repr(response))
+            raise Exception("Chunk verify failed")
+        addr += numbytes
+    # finish the rom transfer if necessary
+    if isRom:
+        print("Finishing ROM upload")
+        romLoadDone(ser)
+    print("All OK")
 
 
 # --------------------
 def usage():
-	print """usage: ser09 [-p device] <action> [file.s19]
+    """usage: ser09 [-p device] <action> [file.s19]
 
 Options:
   -p     specify serial device
@@ -204,93 +230,96 @@ If file's base address is 0xE000, it will be loaded into ROM.
 
 # --------------------
 def load(ser,file):
-	# load the file
-	memimage = MemImage.fromS19File(file)
-	isRom = False
-	if memimage.startAddr == ROM_START:
-		isRom = True
-		if memimage.length() != ROM_LENGTH:
-			raise Exception("ROM image must be exactly %d bytes" % ROM_LENGTH)
-		elif memimage.startAddr > ROM_START:
-			raise Exception("ROM image must start at 0x%04X" % ROM_START)
-	elif memimage.startAddr != RAM_PROG_START:
-		raise Exception("RAM program must start at 0x%04X" % RAM_PROG_START)	
-	# send it
-	sendData(ser, memimage, isRom)
+    # load the file
+    memimage = MemImage.fromS19File(file)
+    isRom = False
+
+#	if memimage.startAddr == ROM_START:
+#		isRom = True
+#		if memimage.length() != ROM_LENGTH:
+#			raise Exception("ROM image must be exactly %d bytes" % ROM_LENGTH)
+#		elif memimage.startAddr > ROM_START:
+#			raise Exception("ROM image must start at 0x%04X" % ROM_START)
+#	elif memimage.startAddr != RAM_PROG_START:
+#		raise Exception("RAM program must start at 0x%04X" % RAM_PROG_START)
+
+    # send it
+    sendData(ser, memimage, isRom)
 
 
 # --------------------
 def run(ser,file):
-	# load it
-	if file:
-		load(ser,file)
-	# then run it
-	execAtAddress(ser, RAM_PROG_START)
+    # load it
+    if file:
+        load(ser,file)
+    # then run it
+    execAtAddress(ser, RAM_PROG_START)
 
 
 # --------------------
 def main(args):
-	try:
-		serialport = getenv("SER09_PORT", 0)
-		baudrate = getenv("SER09_BAUD", 38400)
-		startupdelay = float(getenv("SER09_STARTUP_DELAY", 0))
-		
-		opts, args = getopt.getopt(args, "p:")
-		for o, a in opts:
-			if o == "-p":
-				serialport = a
-			else:
-				assert False, "unhandled option"
-	
-		if len(args) < 1:
-			usage()
-			return 1
-		
-		action = None
-		if args[0] == "load" and len(args) == 2:
-			action = lambda ser: load(ser,args[1])
-		elif args[0] == "run" and len(args) <= 2:
-			if len(args) == 2:
-				action = lambda ser: run(ser,args[1])
-			else:
-				action = lambda ser: run(ser,None)
-		elif args[0] == "read" and len(args) == 3:
-			addr = int(args[1], 16)
-			count = int(args[2], 10)
-			action = lambda ser: readAndPrintMem(ser,addr,count)
-		elif args[0] == "v0":
-			action = lambda ser: readMem(ser,0xCC03,1)
-		elif args[0] == "v1":
-			action = lambda ser: readMem(ser,0xCC05,1)
-		else:
-			usage()
-			return 1
-			
-		if action:
-			ser = serial.Serial(serialport, baudrate)
-			print "Using", ser.portstr
-			
-			# optional delay, in case the remote device is an Arduino
-			# that resets every time the serial port is opened (stupid)
-			if startupdelay:
-				print "Waiting for remote...",
-				sys.stdout.flush()
-				sleep(startupdelay)
-				print "done"
-			
-			action(ser);
-			ser.close()
+    try:
+        serialport = getenv("SER09_PORT", 0)
+        baudrate = getenv("SER09_BAUD", 38400)
+        startupdelay = float(getenv("SER09_STARTUP_DELAY", 0))
 
-	except getopt.GetoptError, e:
-		print str(e)
-		usage()
-		return 1
-	except Exception, e:
-		print >>sys.stderr, e
-		return 2
-		
-	return 0
+        opts, args = getopt.getopt(args, "p:")
+        for o, a in opts:
+            if o == "-p":
+                serialport = a
+            else:
+                assert False, "unhandled option"
+
+        if len(args) < 1:
+            usage()
+            return 1
+
+        action = None
+        if args[0] == "load" and len(args) == 2:
+            action = lambda ser: load(ser,args[1])
+        elif args[0] == "run" and len(args) <= 2:
+            if len(args) == 2:
+                action = lambda ser: run(ser,args[1])
+            else:
+                action = lambda ser: run(ser,None)
+        elif args[0] == "read" and len(args) == 3:
+            addr = int(args[1], 16)
+            count = int(args[2], 10)
+            action = lambda ser: readAndPrintMem(ser,addr,count)
+        elif args[0] == "v0":
+            action = lambda ser: readMem(ser,0xCC03,1)
+        elif args[0] == "v1":
+            action = lambda ser: readMem(ser,0xCC05,1)
+        else:
+            usage()
+            return 1
+
+        if action:
+            ser = serial.Serial(serialport, baudrate)
+            print("Using", ser.portstr)
+
+            # optional delay, in case the remote device is an Arduino
+            # that resets every time the serial port is opened (stupid)
+            if startupdelay:
+                print("Waiting for remote..."),
+                sys.stdout.flush()
+                sleep(startupdelay)
+                print("done")
+
+            action(ser)
+            ser.close()
+
+    except getopt.GetoptError as e:
+        print(str(e))
+        usage()
+        ser.close()
+        return 1
+#    except Exception as e:
+#        print(e, file=sys.stderr)
+#        return 2
+
+    return 0
 
 # --------------------
 if __name__ == "__main__":
-	sys.exit(main(sys.argv[1:]))
+    sys.exit(main(sys.argv[1:]))
